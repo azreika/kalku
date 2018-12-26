d65cfc5e40607808a433c6d166dd4b77a00272e2
Abdul Zreika <azre6702@uni.sydney.edu.au>
2018-12-26 13:16:30 +1100

    Added some boilerplate for lexing.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,56 @@
+#[derive(Debug)]
+enum Operator {
+    Plus,
+    Minus,
+    Multiply,
+}
+
+#[derive(Debug)]
+enum Token<'a> {
+    Operator(Operator),
+    Number(i32),
+    Error(&'a str),
+}
+
+struct Lexer<'a> {
+    program: &'a str,
+    idx: usize,
+}
+
+impl<'a> Lexer<'a> {
+    fn new(program: &str) -> Lexer {
+        Lexer {
+            program: program,
+            idx: 0,
+        }
+    }
+
+    // TODO: implement as an iterator?
+    fn next(&mut self) -> Token {
+        self.idx += 1;
+        Token::Error("not implemented")
+    }
+
+    fn has_next(&self) -> bool {
+        self.idx < self.program.len()
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    run_tests();
+}
+
+fn run_tests() {
+    test_program(&String::from("1"));
+    test_program(&String::from("1+2"));
+    test_program(&String::from("1-2"));
+    test_program(&String::from("1*2"));
+}
+
+fn test_program(program: &str) {
+    println!("Testing program: [{}]", program);
+    let mut lexer = Lexer::new(&program);
+    while lexer.has_next() {
+        println!("TOKEN: {:?}", lexer.next());
+    }
 }
